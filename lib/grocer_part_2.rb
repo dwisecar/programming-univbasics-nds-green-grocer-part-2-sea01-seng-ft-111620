2047131687f3a173e79ea31075d6e40cef919d82
David Wisecarver <dwisecar@gmail.com>
2020-11-09 17:18:37 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,12 +8,13 @@ def apply_coupons(cart, coupons)
     
     if grocery_item[:item] == coupons[0][:item]
       if coupons[0][:num] > grocery_item[:count]
+        binding.pry
         new_item_with_coupon = {:item => "#{grocery_item[:item]} +  W/COUPON", :price => (coupons[0][:cost] / coupons[0][:num]), 
           :clearance => grocery_item[:clearance],
           :count => grocery_item[:count] - coupons[0][:num]}
         cart << new_item_with_coupon
         grocery_item[:count] -= coupons[0][:num]
-        binding.pry
+        
       end
       
     end
